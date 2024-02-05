e74211e839d203f4ac5649e5c153719a5b748dd5
Zkaya9 <zeynepkaya0109@gmail.com>
2024-02-05 11:45:30 -0600

    functions  exercises

--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,55 @@
 function sayHello() {
    console.log("Hello, World!");
 }
+
+
+function makeLine(size){
+   let line = '';
+   for (i = 0 ; i < size; i++){
+      line += '#'
+   }
+   return line;
+}
+console.log(makeLine(5));
+
+
+function makeSquare (size){
+   let square = '';        // initializing  empty to store the  square in
+   for (let i  = 0; i <size; i++){  // itarates through each row of the square
+      square += makeLine(size); // calls makeLine function to  create line of hashes then adds it to square string
+
+      if ( i< size -1){  //adds a new line at the end of each row except last one
+         square += '\n';
+      }
+   }
+return square;
+
+}
+
+console.log(makeSquare(5))
+
+
+function makeRectangle(width, height) {
+   let rectangle = '';
+   for (let i = 0; i < height; i++) {
+     rectangle += (makeLine(width) + '\n');
+   }
+   return rectangle.slice(0, -1);
+ }
+
+ 
+ function makeDownwardStairs(height) {
+   let stairs = '';
+   for (let i = 0; i < height; i++) {
+     stairs += (makeLine(i+1) + '\n');
+   }
+   return stairs.slice(0, -1);
+ }
+
+ function makeIsoscelesTriangle(height) {
+   let triangle = '';
+   for (let i = 0; i < height; i++) {
+     triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+   }
+   return triangle.slice(0, -1);
+ }
